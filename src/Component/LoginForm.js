8b25c79a7340349191c6af640b62dd6d6f42d38d
Anthony Rodriguez <antcreatesmagic@gmail.com>
2020-09-18 01:03:10 -0500

    login post/not working

--- src/Component/LoginForm.js
+++ src/Component/LoginForm.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 
 const FormDiv = styled.form`
@@ -11,22 +12,61 @@ const FormDiv = styled.form`
     width: 80%;
 `;
 
-const LoginForm = () => {
+
+
+const LoginForm = (props) => {
+
+    const [err, setErr] = useState();
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+        console.log(e.target.value)
+    }
+    
+    const handleSubmit = e =>{
+        e.preventDefault()
+        console.log(formData)
+        axios
+        .post( 'https://how-to-app-backend-api.herokuapp.com/api/login', formData)
+        .then( response => {
+            console.log(response)
+        })
+        .catch(err=>{
+            setErr(err.response.data)
+        })
+    }
+
+
     return (
-        <FormDiv>
+        <FormDiv
+        onSubmit = {handleSubmit}
+        >
         <p>Welcome Back!</p>
         <label>Username:
             <input
                 type='text'
+                value={formData.username}
+                name='username'
                 placeholder='Enter Username'
                 maxLength='30'
+                onChange = {handleChange}
             />
         </label>  
         <label>Password:
             <input
                 type='password'
+                value={formData.password}
+                name='password'
                 placeholder='Enter Password'
                 maxLength='20'
+                onChange = {handleChange}
             />
         </label> 
         <button className='submit' >Submit</button>
