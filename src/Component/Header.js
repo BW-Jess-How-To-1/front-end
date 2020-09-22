42f137d211fd707bd17064ed5c29af5824fbd022
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-09-22 19:49:15 -0400

    PostForm gets 404 from api

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 import styled from 'styled-components';
+import FormBox from './FormBox';
+import PostForm from './PostForm';
 
 const Head = styled.div`
     font-family: 'Handlee', cursive;
@@ -9,8 +12,16 @@ const Head = styled.div`
 const Title = () => {
     return (
         <Head>
-            <h1>How To..</h1>
-            <p>This is where some clever description goes.</p>
+            <div>
+                <h1>How To..</h1>
+                <p>This is where some clever description goes.</p>
+            </div>
+            <div>
+                <Link to='/' >Login</Link>
+                <Link to='/post' >Post</Link>
+            </div>
+            <Route exact path="/" component={FormBox} />
+            <Route path="/post" component={PostForm} />
         </Head>
     )
 }
