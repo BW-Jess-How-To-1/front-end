14971073be4c9555d86c0879d5fd8198b5a53b77
noah niesen <noah@neu.local>
2020-09-23 08:59:04 -0500

    installed react-router-dom, wrapped App in router, started development of NavBar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import Tutorials from './Component/Tutorial';
 import './App.css';
 import FormBox from './Component/FormBox';
 
+import NavBar from './Component/NavBar';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-        <Title/>
-        <FormBox/>
-        <Tutorials />
-    </div>
+    <Router>
+      <div className="App">
+          <NavBar />
+          <Title/>
+          <FormBox/>
+          <Tutorials />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Component/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <>
+      <div className='nav-wrapper'>
+        <nav>
+          <Link className='nav-logo-logged-in' to='/'>
+            HOW TO
+          </Link>
+          <div className='menu'>
+            <NavLink exact to ='/features' className='menu-links' activeClassName='active'>
+              Features
+            </NavLink>
+            <NavLink exact to='/team' className='menu-links' activeClassName='active'>
+              Team
+            </NavLink>
+            <NavLink exact to='/login' className='menu-links' activeClassName='active'>
+              Login
+            </NavLink>
+            <NavLink exact to='/signup' className='menu-links' activeClassName='active'>
+              Sign Up
+            </NavLink>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
